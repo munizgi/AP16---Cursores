0c1a4969cf1853db020391882732b624d2cbb41a
Giovanna Muniz <munz.giovanna01@gmail.com>
2026-04-03 20:17:59 -0300

    Exercício 1.2 - Nomes em ordem reversa

--- Exc16.sql
+++ Exc16.sql
@@ -18,11 +18,27 @@ BEGIN
 END;
 $$
 
+-- QUESTÃO 1.2 -  Escreva um cursor que exibe todos os nomes dos youtubers em ordem reversa. Para tal
+-- - O SELECT deverá ordenar em ordem não reversa
+-- - O Cursor deverá ser movido para a última tupla
+-- - Os dados deverão ser exibidos de baixo para cima
 
-
-
-
-
-
-
+DO $$
+DECLARE
+    yt_nome VARCHAR(200);
+    cur_nome REFCURSOR;
+BEGIN
+    OPEN cur_nome SCROLL FOR SELECT youtuber FROM tb_top_youtubers;
+        LOOP
+            FETCH cur_nome INTO yt_nome;
+            EXIT WHEN NOT FOUND;
+        END LOOP;
+        LOOP
+            FETCH BACKWARD FROM cur_nome INTO yt_nome;
+            EXIT WHEN NOT FOUND;
+            RAISE NOTICE '%', yt_nome;
+        END LOOP;
+    CLOSE cur_nome;
+END;
+$$
 
